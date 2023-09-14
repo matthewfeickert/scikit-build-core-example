d2992304761044505e4d68b4c000918b22b991f2
Matthew Feickert <matthew.feickert@cern.ch>
2023-09-14 00:04:51 -0500

    Add outline of package

---
+++ subdir/src/basic_math.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) { return i + j; }
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(basic_math, m) {
+  m.doc() = R"pbdoc(
+      Pybind11 example plugin
+      -----------------------
+      .. currentmodule:: python_example
+      .. autosummary::
+         :toctree: _generate
+         add
+         subtract
+  )pbdoc";
+
+  m.def("add", &add, R"pbdoc(
+      Add two numbers
+      Some other explanation about the add function.
+  )pbdoc");
+}
+
